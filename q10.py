516a6b38a9570da29b061ce382dcc9176f1e8ab6
joaoportelaqqq <joaoplp2507@gmail.com>
2025-04-15 20:38:45 -0300

    Update q10.py

--- q10.py
+++ q10.py
@@ -1,1 +1,8 @@
+salario_bruto = 5000
+inss = 15
+ir = 10
+variavel_inss = (inss/100) * salario_bruto
+variavel_ir = (ir/100) * variavel_inss
+salario_liquido = salario_bruto - variavel_inss - variavel_ir
+print(f"{variavel_inss}\n{variavel_ir}\n{salario_liquido}")
 
